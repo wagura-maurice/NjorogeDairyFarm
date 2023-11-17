7694e1222e3f72e6fe7520ac9c371d2286a1396f
Wagura Maurice <business@waguramaurice.com>
2023-11-18 02:37:53 +0300

    update AppNavigator.tsx

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -7,13 +7,13 @@ import Profile from '../components/common/Profile'; // Import Profile screen
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
-    return (
-      <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
-        <Stack.Screen name="Profile" component={Profile} />
-        {/* Define other screens/routes here */}
-      </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
+      <Stack.Screen name="Profile" component={Profile} />
+      {/* Define other screens/routes here */}
+    </Stack.Navigator>
+  );
 };
 
 export default AppNavigator;
