a9990aa3c5ff654a6439afe6bf4cf2e0be024de2
Wagura Maurice <business@waguramaurice.com>
2023-11-18 02:13:46 +0300

    feat: create AuthContext.tsx

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,102 @@
+// src/context/AuthContext.js
+import React, { createContext, useState, useCallback } from 'react';
+import api from '../utils/api';
+import { storeData, removeData, getData } from '../utils/storage';
+import NotificationModal from '../components/common/NotificationModal'; // Import the NotificationModal
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+  const [modalType, setModalType] = useState('error'); // Type can be 'success', 'warning', 'info', 'danger'
+
+  const showError = (message) => {
+    setModalMessage(message);
+    setModalType('danger');
+    setModalVisible(true);
+  };
+
+  const signIn = useCallback(async (email, password) => {
+    try {
+      const response = await api.post('/auth/sign-in', { email, password });
+      if (response.data.status === 'success') {
+        const userData = response.data.data.user;
+        await storeData('userData', JSON.stringify(userData));
+        await storeData('userToken', response.data.data.accessToken);
+        await storeData('userRoles', JSON.stringify(userData.roles));
+      } else {
+        showError(response.data.message);
+      }
+    } catch (error) {
+      showError('Sign In Error: ' + error.message);
+    }
+  }, []);
+
+  const signUp = useCallback(async (name, email, password, role) => {
+    try {
+      const response = await api.post('/auth/sign-up', { name, email, password, role });
+      if (response.data.status === 'success') {
+        const userData = response.data.data.user;
+        await storeData('userData', JSON.stringify(userData));
+        await storeData('userToken', response.data.data.accessToken);
+        await storeData('userRoles', JSON.stringify(userData.roles));
+      } else {
+        showError(response.data.message);
+      }
+    } catch (error) {
+      showError('Sign Up Error: ' + error.message);
+    }
+  }, []);
+
+  const signOut = useCallback(async () => {
+    try {
+      await removeData('userToken');
+      await removeData('userData');
+      await removeData('userRoles');
+    } catch (error) {
+      showError('Sign Out Error: ' + error.message);
+ + }
+  }, []);
+
+  const forgotPassword = useCallback(async (email) => {
+    try {
+      const response = await api.post('/auth/forgot-password', { email });
+      if (response.data.status === 'success') {
+        console.log(response.data.message);
+      } else {
+        showError(response.data.message);
+      }
+    } catch (error) {
+      showError('Forgot Password Error: ' + error.message);
+    }
+  }, []);
+
+  const hasRole = useCallback(async (roleSlug) => {
+    const roles = await getData('userRoles');
+    if (roles) {
+      const rolesArray = JSON.parse(roles);
+      return rolesArray.some(role => role.slug === roleSlug);
+    }
+    return false;
+  }, []);
+
+  const getUserData = useCallback(async () => {
+    const userData = await getData('userData');
+    return userData ? JSON.parse(userData) : null;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signIn, signUp, signOut, hasRole, getUserData, forgotPassword }}>
+      {children}
+      <NotificationModal
+        isVisible={isModalVisible}
+        type={modalType}
+        message={modalMessage}
+        onClose={() => setModalVisible(false)}
+      />
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
