10d52890cbc09c212afb19584adaf69408e5b0e6
Wagura Maurice <business@waguramaurice.com>
2023-11-17 23:26:39 +0300

    update Profile.tsx

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -1,40 +1,80 @@
 // src/components/common/Profile.tsx
 import React from 'react';
-import { SafeAreaView, View, Text, Pressable } from 'react-native';
-import Avatar from './Avatar';
-import tailwind from 'tailwind-rn';
+import { SafeAreaView, View, Text, Pressable, StyleSheet } from 'react-native';
 import { IoMdHelpBuoy } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
 import { TbLogout } from "react-icons/tb";
+import Avatar from './Avatar';
 
 const Profile = () => {
   return (
-    <SafeAreaView style={tailwind('flex-1 w-full bg-gray-950')}>
-      <View style={tailwind('flex-1 items-center justify-center gap-8')}>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
         <Avatar source={{ uri: "https://source.unsplash.com/random" }} />
-        <View style={tailwind('gap-2 items-center')}>
-          <Text style={tailwind('text-gray-50 text-3xl font-bold')}>
-            Joe Bloggs
-          </Text>
-          <Text style={tailwind('text-gray-50 text-lg')}>joe@bloggs.com</Text>
+        <View style={styles.userInfo}>
+          <Text style={styles.userName}>Joe Bloggs</Text>
+          <Text style={styles.userEmail}>joe@bloggs.com</Text>
         </View>
       </View>
-      <View style={tailwind('flex-1 justify-center gap-8')}>
-        <Pressable style={tailwind('flex-row items-center gap-2 px-8')}>
+      <View style={styles.menuContainer}>
+        <Pressable style={styles.menuItem}>
+          {/* Replace these icons with React Native compatible icons */}
           <IoSettingsOutline name="settings-outline" size={24} color="#fff" />
-          <Text style={tailwind('text-gray-50 text-lg')}>Settings</Text>
+          <Text style={styles.menuText}>Settings</Text>
         </Pressable>
-        <Pressable style={tailwind('flex-row items-center gap-2 px-8')}>
+        <Pressable style={styles.menuItem}>
           <IoMdHelpBuoy name="help-buoy-outline" size={24} color="#fff" />
-          <Text style={tailwind('text-gray-50 text-lg')}>Help</Text>
+          <Text style={styles.menuText}>Help</Text>
         </Pressable>
-        <Pressable style={tailwind('flex-row items-center gap-2 px-8')}>
+        <Pressable style={styles.menuItem}>
           <TbLogout name="logout" size={24} color="#fff" />
-          <Text style={tailwind('text-gray-50 text-lg')}>Logout</Text>
+          <Text style={styles.menuText}>Logout</Text>
         </Pressable>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#1f2937', // Approximation of Tailwind bg-gray-950
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 32,
+  },
+  userInfo: {
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  userName: {
+    color: '#f9fafb', // Approximation of Tailwind text-gray-50
+    fontSize: 24, // Approximation of Tailwind text-3xl
+    fontWeight: 'bold',
+  },
+  userEmail: {
+    color: '#f9fafb', // Approximation of Tailwind text-gray-50
+    fontSize: 18, // Approximation of Tailwind text-lg
+  },
+  menuContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  menuItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 32,
+    paddingVertical: 8,
+  },
+  menuText: {
+    color: '#f9fafb', // Approximation of Tailwind text-gray-50
+    fontSize: 18, // Approximation of Tailwind text-lg
+    marginLeft: 8,
+  },
+});
 
 export default Profile;
