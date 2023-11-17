cb0f88f47a3434b7988bc2baf5ea8564ef020628
Wagura Maurice <business@waguramaurice.com>
2023-11-17 23:09:36 +0300

    feat: create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './navigation/AppNavigator';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default App;
