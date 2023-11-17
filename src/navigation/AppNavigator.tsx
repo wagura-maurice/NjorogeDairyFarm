d278d344fcdf7f82071e810d615ecf2655e3b69f
Wagura Maurice <business@waguramaurice.com>
2023-11-17 23:09:32 +0300

    feat: create AppNavigator.tsx

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,19 @@
+// src/navigation/AppNavigator.tsx
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Splash from '../components/common/SplashScreen';
+import Profile from '../components/common/Profile'; // Import Profile screen
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+    return (
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
+        <Stack.Screen name="Profile" component={Profile} />
+        {/* Define other screens/routes here */}
+      </Stack.Navigator>
+    );
+};
+
+export default AppNavigator;
