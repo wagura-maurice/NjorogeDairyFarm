8ace50df08835fd12c37c207384355cb1c67f9f7
Wagura Maurice <business@waguramaurice.com>
2023-11-18 02:13:56 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
+import { AuthProvider } from './context/AuthContext'; // Import the AuthProvider
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <AuthProvider> {/* Wrap NavigationContainer with AuthProvider */}
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </AuthProvider>
   );
 };
 
