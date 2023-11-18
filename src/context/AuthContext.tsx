177f0cf00d32d3f6d1e81f87ff0b15b0a0784834
Wagura Maurice <business@waguramaurice.com>
2023-11-19 01:25:10 +0300

    update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,34 +2,24 @@
 import React, { createContext, useState, useCallback } from 'react';
 import api from '../utils/API';
 import { storeData, removeData } from '../utils/Storage';
-import NotificationModal from '../components/common/NotificationModal'; // Import the NotificationModal
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isModalVisible, setModalVisible] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
-  const [modalType, setModalType] = useState('error'); // Type can be 'success', 'warning', 'info', 'danger'
-
-  const showError = (message) => {
-    setModalMessage(message);
-    setModalType('danger');
-    setModalVisible(true);
-  };
-
   const signIn = useCallback(async (email, password) => {
     try {
       const response = await api.post('/auth/sign-in', { email, password });
       if (response.data.status === 'success') {
-        const userData = response.data.data.user;
-        await storeData('userData', JSON.stringify(userData));
-        await storeData('userToken', response.data.data.accessToken);
-        await storeData('userRoles', JSON.stringify(userData.roles));
+        const { user, accessToken } = response.data.data;
+        await storeData('userData', JSON.stringify(user));
+        await storeData('userToken', accessToken);
+        await storeData('userRoles', JSON.stringify(user.roles));
+        return 'Sign In Successful';
       } else {
-        showError(response.data.message);
+        throw new Error(response.data.message);
       }
     } catch (error) {
-      showError('Sign In Error: ' + error.message);
+      throw error;
     }
   }, []);
 
@@ -37,25 +27,34 @@ export const AuthProvider = ({ children }) => {
     try {
       const response = await api.post('/auth/sign-up', { name, email, password, role });
       if (response.data.status === 'success') {
-        const userData = response.data.data.user;
-        await storeData('userData', JSON.stringify(userData));
-        await storeData('userToken', response.data.data.accessToken);
-        await storeData('userRoles', JSON.stringify(userData.roles));
+        const { user, accessToken } = response.data.data;
+        await storeData('userData', JSON.stringify(user));
+        await storeData('userToken', accessToken);
+        await storeData('userRoles', JSON.stringify(user.roles));
+        return 'Sign Up Successful';
       } else {
-        showError(response.data.message);
+        throw new Error(response.data.message);
       }
     } catch (error) {
-      showError('Sign Up Error: ' + error.message);
+      throw error;
     }
   }, []);
 
   const signOut = useCallback(async () => {
     try {
-      await removeData('userToken');
+      const userToken = await removeData('userToken'); // Get token and then remove it
+      if (userToken) {
+        await api.post('/auth/sign-out', {}, {
+          headers: {
+            Authorization: `Bearer ${userToken}`
+          }
+        });
+      }
       await removeData('userData');
       await removeData('userRoles');
+      return 'Sign Out Successful';
     } catch (error) {
-      showError('Sign Out Error: ' + error.message);
+      throw error;
     }
   }, []);
 
@@ -63,24 +62,18 @@ export const AuthProvider = ({ children }) => {
     try {
       const response = await api.post('/auth/forgot-password', { email });
       if (response.data.status === 'success') {
-        console.log(response.data.message);
+        return response.data.message;
       } else {
-        showError(response.data.message);
+        throw new Error(response.data.message);
       }
     } catch (error) {
-      showError('Forgot Password Error: ' + error.message);
+      throw error;
     }
   }, []);
 
   return (
     <AuthContext.Provider value={{ signIn, signUp, signOut, forgotPassword }}>
       {children}
-      <NotificationModal
-        isVisible={isModalVisible}
-        type={modalType}
-        message={modalMessage}
-        onClose={() => setModalVisible(false)}
-      />
     </AuthContext.Provider>
   );
 };
