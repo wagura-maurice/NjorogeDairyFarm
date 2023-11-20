114642894e50fc571b8dfb6c9b14d37ddec751d4
Wagura Maurice <business@waguramaurice.com>
2023-11-20 17:39:35 +0300

    feat: create Dashboard.tsx

---
+++ src/components/administrator/Dashboard.tsx
@@ -0,0 +1,5 @@
+// src/components/administrator/Dashboard.tsx
+
+generate the code for this file, in a way i have cards for each of the components i have created, and i can click on them to go to the page.
+
+the pages are 
