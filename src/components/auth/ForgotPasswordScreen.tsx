7532549758f88cbd40d0e68e95942eb1489e88d3
Wagura Maurice <business@waguramaurice.com>
2024-01-01 11:02:55 +0300

    update ForgotPasswordScreen.tsx, SignInScreen.tsx and SignUpScreen.tsx

--- src/components/auth/ForgotPasswordScreen.tsx
+++ src/components/auth/ForgotPasswordScreen.tsx
@@ -1,5 +1,5 @@
 // src/components/auth/ForgotPasswordScreen.tsx
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 import {
   View,
   Text,
@@ -9,35 +9,36 @@ import {
   KeyboardAvoidingView,
   Platform,
   Image,
-} from 'react-native';
-import { AuthContext } from '../../context/AuthContext';
-import NotificationModal from '../common/NotificationModal';
-import { validateEmail } from '../../utils/Validation';
+} from "react-native";
+import { AuthContext } from "../../context/AuthContext";
+import NotificationModal from "../common/NotificationModal";
+import { validateEmail } from "../../utils/Validation";
 
 const ForgotPasswordScreen = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
+  const [modalMessage, setModalMessage] = useState("");
   const { forgotPassword } = useContext(AuthContext);
 
-  const [modalType, setModalType] = useState('info'); // New state for modal type
+  const [modalType, setModalType] = useState("info"); // New state for modal type
 
   const handleForgotPassword = async () => {
-    if (!validateEmail(email)) { // Use the validateEmail function for robust email validation
-      setModalMessage('Please enter a valid email address.');
-      setModalType('warning'); // Set the modal type to warning
+    if (!validateEmail(email)) {
+      // Use the validateEmail function for robust email validation
+      setModalMessage("Please enter a valid email address.");
+      setModalType("warning"); // Set the modal type to warning
       setModalVisible(true);
       return;
     }
 
     try {
       const message = await forgotPassword(email);
-      setModalMessage(message || 'Password reset email sent.');
-      setModalType('success'); // Set the modal type to success
+      setModalMessage(message || "Password reset email sent.");
+      setModalType("success"); // Set the modal type to success
       setModalVisible(true);
     } catch (error) {
       setModalMessage(error.message);
-      setModalType('danger'); // Set the modal type to danger on error
+      setModalType("danger"); // Set the modal type to danger on error
       setModalVisible(true);
     }
   };
@@ -49,14 +50,15 @@ const ForgotPasswordScreen = () => {
     >
       <View style={styles.content}>
         <View style={styles.circle}>
-          <Image 
-            source={require('../../assets/img/logo.png')} 
-            style={styles.logo} 
+          <Image
+            source={require("../../assets/img/logo.png")}
+            style={styles.logo}
             resizeMode="contain"
           />
         </View>
         <Text style={styles.subtitle}>
-          Forgotten your account password? Enter your email address below and you'll receive a link to create a new one.
+          Forgotten your account password? Enter your email address below and
+          you'll receive a link to create a new one.
         </Text>
         <TextInput
           style={styles.input}
@@ -84,60 +86,60 @@ const ForgotPasswordScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0ebe6', // A soft cream background
+    backgroundColor: "#f0ebe6", // A soft cream background
   },
   content: {
     padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
   },
   circle: {
     width: 100,
     height: 100,
     borderRadius: 50, // Half of width and height to make it a circle
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderWidth: 5,
-    borderColor: 'green',
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'hidden', // This ensures the image does not escape the circle boundaries
+    borderColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden", // This ensures the image does not escape the circle boundaries
     marginBottom: 20, // Space between circle and text
   },
   logo: {
-    width: '100%', // Fill the circle
-    height: '100%', // Maintain aspect ratio
+    width: "100%", // Fill the circle
+    height: "100%", // Maintain aspect ratio
     marginLeft: -1.5, // Move the logo to the left by 1px
     marginTop: -1.5, // Move the logo to the top by 1px
   },
   subtitle: {
     fontSize: 12,
-    color: '#555', // Darker grey for subtitle
+    color: "#555", // Darker grey for subtitle
     marginBottom: 30,
-    textAlign: 'center',
+    textAlign: "center",
   },
   input: {
-    width: '100%',
+    width: "100%",
     borderWidth: 1,
-    borderColor: '#333',
-    backgroundColor: '#fff',
+    borderColor: "#333",
+    backgroundColor: "#fff",
     padding: 15,
     marginBottom: 15,
     borderRadius: 5,
-    color: '#333',
+    color: "#333",
   },
   button: {
-    width: '100%',
-    backgroundColor: '#00FF00',
+    width: "100%",
+    backgroundColor: "#00FF00",
     padding: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 5,
     marginBottom: 20,
   },
   buttonText: {
-    color: '#000',
-    fontWeight: 'bold',
+    color: "#000",
+    fontWeight: "bold",
     fontSize: 16,
   },
 });
--- src/components/auth/SignInScreen.tsx
+++ src/components/auth/SignInScreen.tsx
@@ -1,35 +1,42 @@
 // src/components/auth/SignInScreen.tsx
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { AuthContext } from '../../context/AuthContext';
-import NotificationModal from '../common/NotificationModal';
-import { validateEmail } from '../../utils/Validation';
-import CheckBox from '@react-native-community/checkbox';
+import React, { useState, useContext } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { AuthContext } from "../../context/AuthContext";
+import NotificationModal from "../common/NotificationModal";
+import { validateEmail } from "../../utils/Validation";
+import CheckBox from "@react-native-community/checkbox";
 
 const SignInScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
+  const [modalMessage, setModalMessage] = useState("");
   const { signIn } = useContext(AuthContext);
   const navigation = useNavigation();
 
   const handleSignIn = async () => {
     if (!email || !validateEmail(email)) {
-      setModalMessage('Please enter a valid email address.');
+      setModalMessage("Please enter a valid email address.");
       setModalVisible(true);
       return;
     }
     if (!password) {
-      setModalMessage('Please enter your password.');
+      setModalMessage("Please enter your password.");
       setModalVisible(true);
       return;
     }
     try {
       await signIn(email, password);
-      navigation.navigate('MarketplaceScreen');
+      navigation.navigate("MarketplaceScreen");
     } catch (error) {
       setModalMessage(error.message);
       setModalVisible(true);
@@ -37,19 +44,19 @@ const SignInScreen = () => {
   };
 
   const navigateToForgotPassword = () => {
-    navigation.navigate('ForgotPasswordScreen');
+    navigation.navigate("ForgotPasswordScreen");
   };
 
   const navigateToSignUp = () => {
-    navigation.navigate('SignUpScreen');
+    navigation.navigate("SignUpScreen");
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.circle}>
-        <Image 
-          source={require('../../assets/img/logo.png')} 
-          style={styles.logo} 
+        <Image
+          source={require("../../assets/img/logo.png")}
+          style={styles.logo}
           resizeMode="contain"
         />
       </View>
@@ -75,7 +82,7 @@ const SignInScreen = () => {
           value={rememberMe}
           onValueChange={setRememberMe}
           style={styles.checkbox}
-          tintColors={{ true: '#00FF00', false: '#333' }} // Optional: customize checkbox colors
+          tintColors={{ true: "#00FF00", false: "#333" }} // Optional: customize checkbox colors
         />
         <Text style={styles.label}>Remember me</Text>
       </View>
@@ -101,70 +108,70 @@ const SignInScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0ebe6',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0ebe6",
     padding: 20,
   },
   circle: {
     width: 100,
     height: 100,
     borderRadius: 50, // Half of width and height to make it a circle
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderWidth: 5,
-    borderColor: 'green',
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'hidden', // This ensures the image does not escape the circle boundaries
+    borderColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden", // This ensures the image does not escape the circle boundaries
     marginBottom: 20, // Space between circle and text
   },
   logo: {
-    width: '100%', // Fill the circle
-    height: '100%', // Maintain aspect ratio
+    width: "100%", // Fill the circle
+    height: "100%", // Maintain aspect ratio
     marginLeft: -1.5, // Move the logo to the left by 1px
     marginTop: -1.5, // Move the logo to the top by 1px
   },
   input: {
-    width: '100%',
+    width: "100%",
     borderWidth: 1,
-    borderColor: '#333',
-    backgroundColor: '#fff',
+    borderColor: "#333",
+    backgroundColor: "#fff",
     padding: 15,
     marginBottom: 15,
     borderRadius: 5,
-    color: '#333',
+    color: "#333",
   },
   checkboxContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 20,
-    alignItems: 'center', // Ensure checkbox and label are vertically aligned
+    alignItems: "center", // Ensure checkbox and label are vertically aligned
   },
   checkbox: {
     marginRight: 8,
   },
   label: {
-    color: '#333',
+    color: "#333",
     marginLeft: 8, // Add space between checkbox and label
   },
   button: {
-    width: '100%',
-    backgroundColor: '#00FF00',
+    width: "100%",
+    backgroundColor: "#00FF00",
     padding: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 5,
     marginBottom: 20,
   },
   buttonText: {
-    color: '#000',
-    fontWeight: 'bold',
+    color: "#000",
+    fontWeight: "bold",
     fontSize: 16,
   },
   resetPassword: {
-    color: '#333',
+    color: "#333",
   },
   signUp: {
-    color: '#333',
+    color: "#333",
     marginTop: 10,
   },
 });
