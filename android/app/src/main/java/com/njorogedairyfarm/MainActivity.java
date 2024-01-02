fe462611d49e7755d8ee99242a638818da6076f8
Wagura Maurice <business@waguramaurice.com>
2024-01-02 18:57:48 +0300

    update MainActivity.java

--- android/app/src/main/java/com/njorogedairyfarm/MainActivity.java
+++ android/app/src/main/java/com/njorogedairyfarm/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "NjorogeDairyFarm";
+    return "Njoroge Dairy Farm";
   }
 
   /**
