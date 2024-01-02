926469a712595fa99795a96234f30f2758e6a721
Wagura Maurice <business@waguramaurice.com>
2024-01-02 18:58:44 +0300

    update Validation.tsx

--- src/utils/Validation.tsx
+++ src/utils/Validation.tsx
@@ -1,7 +1,12 @@
 // src/utils/Validation.tsx
-const emailRegex =
-  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z0-9\-_]+\.)+[a-zA-Z]{2,}))$/;
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z0-9\-_]+\.)+[a-zA-Z]{2,}))$/;
+
+const telephoneRegex = /^\+2547\d{8}$|^07\d{8}$/;
 
 export const validateEmail = (email: string) => {
   return emailRegex.test(email);
+};
+
+export const validateTelephoneNumber = (phone: string) => {
+  return telephoneRegex.test(phone);
 };
